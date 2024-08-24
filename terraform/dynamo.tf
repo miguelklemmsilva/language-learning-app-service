083d4cef3e1674af73a571c0f20a9f94c713890c
Miguel Klemm Silva <m.klemmsilva@lancaster.ac.uk>
2024-08-24 15:24:32 +0100

    add dynamo table

---
+++ terraform/dynamo.tf
@@ -0,0 +1,20 @@
+resource "aws_dynamodb_table" "users" {
+  name         = "users"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "UserId"
+  
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+  
+  attribute {
+    name = "Email"
+    type = "S"
+  }
+  
+  attribute {
+    name = "ActiveLanguage"
+    type = "S"
+  }
+}
