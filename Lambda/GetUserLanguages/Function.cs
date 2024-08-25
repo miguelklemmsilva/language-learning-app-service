ca9a13f24791bc779f40150cb038a1166b2af855
Miguel Klemm Silva <m.klemmsilva@lancaster.ac.uk>
2024-08-25 22:19:36 +0100

    [build] add delete as an option

--- Lambda/GetUserLanguages/Function.cs
+++ Lambda/GetUserLanguages/Function.cs
@@ -24,7 +24,7 @@ public class Function
     {
         { "Content-Type", "application/json" },
         { "Access-Control-Allow-Origin", "*" },
-        { "Access-Control-Allow-Methods", "GET,POST,OPTIONS" },
+        { "Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE" },
         { "Access-Control-Allow-Headers", "Content-Type" }
     };
 
