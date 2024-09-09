2c71cf74571a7e958c74196996eca27bacaf176a
Miguel Klemm Silva <m.klemmsilva@lancaster.ac.uk>
2024-09-09 17:54:10 +0100

    change return type

--- Lambda/LambdaStartup/Function.cs
+++ Lambda/LambdaStartup/Function.cs
@@ -289,7 +289,7 @@ public class Function(
         {
             var categories = await allowedVocabularyService.GetWordsByCategoryAsync(language);
             
-            return ResponseHelper.CreateSuccessResponse(categories, typeof(IDictionary<string, IEnumerable<string>>));
+            return ResponseHelper.CreateSuccessResponse(categories, typeof(IEnumerable<Category>));
         }
         catch (Exception e)
         {
