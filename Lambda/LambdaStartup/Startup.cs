0eff8edcbf6f427c997482cb8b35e9efb2af67d7
Miguel Klemm Silva <m.klemmsilva@lancaster.ac.uk>
2025-03-22 13:58:10 +0000

    assert chat gpt key is not null

--- Lambda/LambdaStartup/Startup.cs
+++ Lambda/LambdaStartup/Startup.cs
@@ -40,9 +40,7 @@ public class Startup
 
     private static void ConfigureChatGptHttpClient(HttpClient client)
     {
-        var chatGptKey = Environment.GetEnvironmentVariable("CHAT_GPT_KEY");
-        if (string.IsNullOrEmpty(chatGptKey))
-            throw new InvalidOperationException("Chat GPT key is missing");
+        var chatGptKey = Environment.GetEnvironmentVariable("CHAT_GPT_KEY")!;
 
         client.BaseAddress = new Uri("https://api.openai.com");
         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
