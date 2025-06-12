b479fdc2fa657e90c60ffbca9a2926200d83cada
Miguel Klemm Silva <m.klemmsilva@lancaster.ac.uk>
2025-06-12 14:17:03 +0100

    ALLOW_USER_SRP_AUTH

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -53,6 +53,7 @@ resource "aws_cognito_user_pool_client" "userpool_client" {
   # Authentication Flows
   explicit_auth_flows = [
     "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_USER_SRP_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH"
   ]
 
